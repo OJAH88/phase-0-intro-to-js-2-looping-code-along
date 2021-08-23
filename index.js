fb28687cf38407aaf93d897062f902f40c2a5a4e
Jesse Harlan <jharlan628@gmail.com>
2021-08-23 18:08:37 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const card = ['Lisa', 'Kaitlin', 'Jan'];
+const thankYou = []
+function writeCards(card){
+for (let counter = 0;counter < 3; ++counter){
+thankYou.push (`Thank you, ${card[counter]}, for the wonderful surprise gift!`);
+}
+return thankYou
+}
+
+let countNum =[]
+function countDown(i){
+while(i>= 0 ){
+    console.log (i);
+    i--;
+}
+return countNum
+}
+
+debugger
